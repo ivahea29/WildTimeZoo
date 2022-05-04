34597480578da74847605be7c351fa5800cea5c7
georgiazrk <gzrkearns@gmail.com>
2022-05-04 18:25:07 +1200

    navFooterupdate, javascript screated for navbar and footer

---
+++ javascript/footer.js
@@ -0,0 +1,16 @@
+document.write('<footer><div class="footer-container">'+
+    '<div class="footer_column">'+
+    '<div class="footer_column_header">SUPPORT US</div>'+
+    '<a href="tickets.html" class="footer_column_link">Tickets</a>'+
+    '<div class="footer_column_link">Donate</div>'+
+    '<div class="footer_column_link">Shop</div></div>'+
+    '<div class="footer_column">'+
+    '<div class="footer_column_header">EDUCATION</div>'+
+    '<div class="footer_column_link">Meet the Animals</div>'+
+    '<a href="visitUs.html" class="footer_column_link">Visit Us</a>'+
+    '<div class="footer_column_link">School Visits</div></div>'+
+    '<div class="footer_column">'+
+    '<div class="footer_column_header">ABOUT THE ZOO</div>'+
+    '<a href="aboutUs.html" class="footer_column_link">About Us</a>'+
+    '<a href="contactUs.html" class="footer_column_link">Contact Us</a></div></div>'+
+    '<div class="footer-bottom">&#169; 2022 North Shore Zoo | Terms of Use | Privacy Policy</div></footer>')
---
+++ javascript/navbar.js
@@ -0,0 +1,6 @@
+document.write('<ul class="nav-list"> ' +
+    '<li class="list-item"><a href="tickets.html">Tickets</a></li>'+
+    '<li class="list-item"><a href="visitUs.html">Visit Us</a></li>'+
+    '<li class="list-item"><a href="contactUs.html">Contact Us</a></li>'+
+    '<li class="list-item"><a>Animals</a></li>'+
+    '<li class="list-item"><a>About Us</a></li></ul></nav>')
