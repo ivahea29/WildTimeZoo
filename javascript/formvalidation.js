27c68cc87d7a28c084bdb9f523c3083671b7dd1b
georgiazrk <gzrkearns@gmail.com>
2022-05-16 17:50:19 +1200

    Final commenting and ticket page updates.

--- javascript/formvalidation.js
+++ javascript/formvalidation.js
@@ -6,6 +6,7 @@ function clearForm(bookingform){
 // date validation function
 
 function validDateCheck(cardExp){
+    //get todays date for comparison to date input.
     var today = new Date();
     var dd = today.getDate();
     var mm = today.getMonth() + 1;
@@ -21,6 +22,7 @@ function validDateCheck(cardExp){
 
     today = yyyy + '-' + mm + '-' + dd;
 
+    // if the card exp date is null, less than or equal to todays date return false. or else return true.
     if (cardExp === "" || cardExp < today || cardExp === today){
         return false;
     }else{
@@ -28,16 +30,19 @@ function validDateCheck(cardExp){
     }
 
 }
-// date input function
 
+// date input function
 function dateInput() {
-    var cardExp = document.getElementById("cardExp").value;
+    var cardExp = document.getElementById("cardExp").value; // get card expiration value
 
     if (cardExp !== ""){
         var validDate = validDateCheck(cardExp);
 
+        // if card exp is not null then assign that value to the variable
         if (cardExp !== ""){
             var validDate = validDateCheck(cardExp);
+
+            // if the date is not valid then display error message
             if(!validDate){
                 document.getElementById("errorMessage").style.display = "block";
             }else{
@@ -55,7 +60,7 @@ function dateInput() {
         }
     }
 }
-// addes values for price function
+// get values dependant on which ticket is input
 
 function getPrice(type){
     var obj = document.getElementById(type);
@@ -64,11 +69,13 @@ function getPrice(type){
         price = 0;
     return parseInt(price);
 }
+// update price function as ticket types are input and display according to values
 function updatePrice(){
     document.getElementById("price").innerHTML = "Total: $" + (getPrice("daypass"));
     document.getElementById("price").innerHTML = "Total: $" + (getPrice("annualpass") + getPrice("daypass"));
 }
-// final submit function
+
+// final submit function to validate all inputs and clear form once purchased.
 
 function submitForm(){
     var fname = document.getElementById("fname").value;
--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,4 +1,5 @@
-document.write('<nav class="navbar"><div><a href="index.html"><img class="logo" src="images\\zooLogofinal1.png" alt="Home"></a></div><ul class="nav-list"> ' +
+//nav bar javascript
+document.write('<nav class="navbar"><div><a href="index.html"><img class="logo" src="images\\zooLogofinal1.png" alt="Home"></a></div><ul class="nav-list"> ' + 
     '<li class="list-item"><a href="tickets.html">Tickets</a></li>'+
     '<li class="list-item"><a href="visitUs.html">Visit Us</a></li>'+
     '<li class="list-item"><a href="contactUs.html">Contact Us</a></li>'+
